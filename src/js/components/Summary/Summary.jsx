4d30e645c91267eaec7517e9c18bcc220854dbdc
Jen <jenniferlynnebarry@gmail.com>
2022-08-22 14:57:30 -0700

    first commit

---
+++ src/js/components/Summary/Summary.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class Summary extends React.Component {
+    render() {
+        return (
+            <div className='card border-info mb03'>
+                <div className='card-header text-white bg-info'>Summary</div>
+                    <div className='card-body'>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col-6 text-center'>
+                                    <h6 className='h6 strong'>Total Income</h6>
+                                        <p>$1,500.00</p>
+                                </div>
+                            </div>
+                            <div className='row justify-content-center'>
+                                <div className='col-6 text-center'>
+                                    <h6 className='h6 strong'>Left after spending</h6>
+                                        <p>$1,500.00</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        );
+    }
+}
