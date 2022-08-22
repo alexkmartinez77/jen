0c1bf8bcffe765520191b63336f7e973dc75f0d0
Jen <jenniferlynnebarry@gmail.com>
2022-08-22 16:54:56 -0700

    connecting the individual components using connect() function

--- src/js/components/IncomeEntries/index.js
+++ src/js/components/IncomeEntries/index.js
@@ -1,3 +1,12 @@
+import { connect } from 'react-redux';
 import IncomeEntries from './IncomeEntries';
 
-export default IncomeEntries;
+function mapStoreToProps(store) {
+	return {
+		description: store.expense.description,
+		amount: store.expense.amount,
+		lineItems: store.expense.lineItems
+	};
+}
+
+export default connect(mapStoreToProps)(IncomeEntries);
