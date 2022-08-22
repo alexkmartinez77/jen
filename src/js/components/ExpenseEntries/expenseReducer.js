b373d6a0a72e1e8caa49269d09b40077e4aa88ba
Jen <jenniferlynnebarry@gmail.com>
2022-08-22 16:35:10 -0700

    Creating and Connecting Reducers

---
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -0,0 +1,10 @@
+const defaultState = {
+	description: '',
+	amount: '',
+	lineItems: []
+};
+
+export default function ExpenseReducer (state = defaultState, action) {
+	// `state = defaultState` is ES6 syntax for defining a default value on a parameter
+	return state;
+}
